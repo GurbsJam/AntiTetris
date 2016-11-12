e6821d721fba6f3fdc473931cabe2b3f0b1a5cfa
Bearded Baron <beardedbaron@gaboduarte.com>
2016-11-12 17:55:08 -0200

    Added variables, working on the upgrade UI

---
+++ Assets/UpgradeUIController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UpgradeUIController : MonoBehaviour {
+
+	public Text tapDamageTitle;
+	public Text tapDamageLevel;
+	public Text tapDamagePrice;
+
+	public Text moneyPerBrickTitle;
+	public Text moneyPerBrickLevel;
+	public Text moneyPerBrickPrice;
+
+	public Text shakeDamageTitle;
+	public Text shakeDamageLevel;
+	public Text shakeDamagePrice;
+
+	public Text shakeTimeTitle;
+	public Text shakeTimeLevel;
+	public Text shakeTimePrice;
+
+	// Use this for initialization
+	void Start () {
+		UpdateUIValues();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void UpdateUIValues ()
+	{
+		tapDamageLevel.text = "Level " + ResourceController.damageUpgradeLevel;
+		tapDamageLevel.text = ResourceController.damageUpgradeCostBase;
+
+		moneyPerBrickLevel.text = "Level " + ResourceController.moneyPerBrickUpgradeLevel;
+		moneyPerBrickPrice.text =
+
+		shakeDamageLevel.text = "Level " + ResourceController.
+		shakeDamagePrice.text = 
+
+		shakeTimeLevel.text = "Level " + ResourceController.
+		shakeTimePrice.text =
+	}
+}
--- Assets/_Scripts/ResourceController.cs
+++ Assets/_Scripts/ResourceController.cs
@@ -5,14 +5,14 @@ public class ResourceController : MonoBehaviour {
 
 	//Money
 	[Header("Money")]
-	public int moneyAmount;
-	public int moneyPerBrick;
-	public int moneyPerBrickUpgradeLevel;
-	public int moneyPerBrickUpgrageBase;
-	public float moneyPerBrickUpgradeMultiplier;
-	public int moneyPerBrickUpgradeCostBase;
-	public float moneyPerBrickUpgradeCostMultiplier;
-	public int initialMoney;
+	public static int moneyAmount;
+	public static int moneyPerBrick;
+	public static int moneyPerBrickUpgradeLevel;
+	public static int moneyPerBrickUpgrageBase;
+	public static float moneyPerBrickUpgradeMultiplier;
+	public static int moneyPerBrickUpgradeCostBase;
+	public static float moneyPerBrickUpgradeCostMultiplier;
+	public static int initialMoney;
 
 	//TODO: Leveling up and stuff...
 	//public int level;
@@ -25,17 +25,27 @@ public class ResourceController : MonoBehaviour {
 
 	//Damage
 	[Header("Damage")]
-	public int damageAmount;
-	public int damageUpgradeLevel;
-	public int damageUpgrageBase;
-	public float damageUpgradeMultiplier;
-	public int damageUpgradeCostBase;
-	public float damageUpgradeCostMultiplier;
+	public static int damageAmount;
+	public static int damageUpgradeLevel;
+	public static int damageUpgrageBase;
+	public static float damageUpgradeMultiplier;
+	public static int damageUpgradeCostBase;
+	public static float damageUpgradeCostMultiplier;
 
 	//Skill
 	[Header("Skill")]
-	public int skill1Damage;
-	public int skill1Recharge;
+	public static int skill1Damage;
+	public static int skill1DamageUpgradeBase;
+	public static int skill1DamageUpgradeMultiplier;
+	public static int skill1DamageUpgradeCostBase;
+	public static int skill1DamageUpgradeCostMultiplier;
+
+	public static int skill1Recharge;
+	public static int skill1RechargeUpgradeBase;
+	public static int skill1RechargeUpgradeMultiplier;
+	public static int skill1RechargeUpgradeCostBase;
+	public static int skill1RechargeUpgradeCostMultiplier;
+
 
 	void addMoney ()
 	{
@@ -48,13 +58,9 @@ public class ResourceController : MonoBehaviour {
 	//	xxx
 	//}
 
-
-
-
-
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
