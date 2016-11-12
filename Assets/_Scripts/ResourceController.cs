c0c86990f9827811f5225f837434707734189588
Bearded Baron <beardedbaron@gaboduarte.com>
2016-11-12 18:55:45 -0200

    Methods are ok now

--- Assets/_Scripts/ResourceController.cs
+++ Assets/_Scripts/ResourceController.cs
@@ -6,15 +6,15 @@ public class ResourceController : MonoBehaviour {
 	//Money
 	[Header("Money")]
 	public static int moneyAmount;
-	public static int moneyPerBrick;
+	public static int initialMoney;
+
 	public static int moneyPerBrickUpgradeLevel;
 	public static int moneyPerBrickUpgrageBase;
 	public static float moneyPerBrickUpgradeMultiplier;
 	public static int moneyPerBrickUpgradeCostBase;
 	public static float moneyPerBrickUpgradeCostMultiplier;
-	public static int initialMoney;
 
-	//TODO: Leveling up and stuff...
+	//TODO: XP - Leveling up and stuff...
 	//public int level;
 	//public int xp;
 	//public float xpMultiplier;
@@ -25,23 +25,20 @@ public class ResourceController : MonoBehaviour {
 
 	//Damage
 	[Header("Damage")]
-	public static int damageAmount;
 	public static int damageUpgradeLevel;
 	public static int damageUpgrageBase;
 	public static float damageUpgradeMultiplier;
-	public static int damageUpgradeCostBase;
+	public static int damageUpgradeCostBase; 
 	public static float damageUpgradeCostMultiplier;
 
 	//Skill
 	[Header("Skill")]
-	public static int skill1Damage;
 	public static int skill1DamageUpgradeLevel;
 	public static int skill1DamageUpgradeBase;
 	public static int skill1DamageUpgradeMultiplier;
 	public static int skill1DamageUpgradeCostBase;
 	public static int skill1DamageUpgradeCostMultiplier;
 
-	public static int skill1Recharge;
 	public static int skill1RechargeUpgradeLevel;
 	public static int skill1RechargeUpgradeBase;
 	public static int skill1RechargeUpgradeMultiplier;
@@ -49,24 +46,60 @@ public class ResourceController : MonoBehaviour {
 	public static int skill1RechargeUpgradeCostMultiplier;
 
 
+	//Get methods:
+	int getMoneyPerBrick ()
+	{
+		return 10; //Fixed for now...
+	}
+
+	int getMoneyPerBrickUpgradePrice ()
+	{
+		return 10; //Fixed for now...
+	}
+
+	int getDamage ()
+	{
+		return 10; //Fixed for now...
+	}
+
+	int getDamageUpgradePrice ()
+	{
+		return 10; //Fixed for now...
+	}
+
+
+	int getSkill1Damage ()
+	{
+		return 10; //Fixed for now...
+	}
+
+	int getSkill1DamageUpgradePrice ()
+	{
+		return 10; //Fixed for now...
+	}
+
+	int getSkill1Recharge ()
+	{
+		return 10; //Fixed for now...
+	}
+
+	int getSkill1RechargeUpgradePrice ()
+	{
+		return 10; //Fixed for now...
+	}
+
+
+
+	//Change Money inventory
+
 	void addMoney ()
 	{
-		moneyAmount += moneyPerBrick;
+		moneyAmount += getMoneyPerBrick();
 	}
 
-	//TODO: Add XP
-	//void addXP
-	//{
-	//	xxx
-	//}
+	void buyUpgrade ()
+	{
+		//TODO: for later!
+	}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
